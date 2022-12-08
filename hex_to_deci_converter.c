7b430c28c3f29a31b6864392d5f86b56d963cb04
O_ <104556158+Bishamonka@users.noreply.github.com>
2022-12-08 15:19:23 +0200

    Add files via upload

---
+++ hex_to_deci_converter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  printf("\n======================================\n\n");
+  printf("Welcome to 'Hex to Deci Converter'\n");
+  printf("Enter a number: \n");
+
+  int the_number;
+  scanf("%d", &the_number);
+  
+  printf("Decimal representation:     %9d\n", the_number);
+  printf("Hexadecimal representation: %9x\n", the_number);
+  
+  printf("\n======================================\n");
+
+  return 0;
+}
